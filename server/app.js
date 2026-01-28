0ec20bb98ceff317e2ec115808cabdc7c61a15c3
Himanshu Mishra <himanshumishra4926@gmail.com>
2026-01-29 03:16:57 +0530

    changes in app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ import fileUpload from 'express-fileupload';
 
 const app = express();
 
-config({ path: './config/.env' });
+config({ path: './config/config.env' });
 
 app.use(cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
