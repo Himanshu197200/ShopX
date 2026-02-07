368f0d386cf583c32f821fb316234b237ffbcdef
Himanshu Mishra <himanshumishra4926@gmail.com>
2026-02-07 17:18:09 +0530

    Created authController and authRoutes for getUSer successfully

--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { register, login, getUser } from "../controllers/authController.js";
+import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/me", isAuthenticated, getUser);
 
 export default router;
