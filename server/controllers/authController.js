265d6bbeb60f83def1f32fe84aec87748b25e341
Himanshu Mishra <himanshumishra4926@gmail.com>
2026-02-07 17:27:09 +0530

    Created authController and authRoutes for logout successfully

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -56,3 +56,16 @@ export const getUser = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
+export const logout = catchAsyncErrors(async (req, res, next) => {
+  res
+    .status(200)
+    .cookie("token", "", {
+      expires: new Date(Date.now()),
+      httpOnly: true,
+    })
+    .json({
+      success: true,
+      message: "Logged out successfully.",
+    });
+});
+
--- server/router/authRoutes.js
+++ server/router/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login, getUser } from "../controllers/authController.js";
+import { register, login, getUser, logout } from "../controllers/authController.js";
 import { isAuthenticated } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", isAuthenticated, getUser);
+router.get("/logout", isAuthenticated, logout);
 
 export default router;
